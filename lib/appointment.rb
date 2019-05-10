c552528ffb891eac369fbc1298dd4b2117c2f217
Garth Lilly <garff247@gmail.com>
2019-05-10 22:37:27 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -8,8 +8,6 @@ attr_accessor :date, :patient, :doctor
     @date = date
     @patient = patient
     @doctor = doctor
-    @patient.appointments << self
-    @doctor.appointments << self
     @@all << self
   end
 
