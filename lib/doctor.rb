63197eb5cdf4608b9d85aa25deee7011831a2d14
Garth Lilly <garff247@gmail.com>
2019-05-10 21:58:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,8 +6,31 @@ class Doctor
 
   def initialize(name)
     @name = name
+    @appointments = []
+    @patients = []
     @@all << self
   end
-  
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.each do |appointment|
+      if appointment.doctor == self
+        @appointments << appointment
+      end
+    end
+    @appointment
+  end
+
+  def patients
+    Appointment.all.each do |appointment|
+      if appointment.doctor == self
+        @patients << appointment.patient 
+      end
+    end
+    @patients
+  end
 
 end
