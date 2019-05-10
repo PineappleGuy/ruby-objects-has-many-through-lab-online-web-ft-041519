c6bc983c7351f7e296d00b11c9970b386ff3ab1f
Garth Lilly <garff247@gmail.com>
2019-05-10 21:20:05 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,6 +1,27 @@
 class Genre
-  attr_accessor :genre
-  def initialize(genre)
-    @genre = genre
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
   end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all do |song|
+      song.genre = self
+    end
+
+  end
+
+  def artists
+    Song.all do |song|
+      
+  end
+
 end
